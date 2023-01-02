a073aa03d48c959fea49f399d50e0cb348338606
Gary Polhill <gary.polhill@hutton.ac.uk>
2023-01-02 23:11:18 +0000

    Allowed output customization

--- README.md
+++ README.md
@@ -10,11 +10,37 @@ Command-line options:
   +	`--user-id-prefix` _P_: use _P_ as a prefix in front of user ID numbers (default `"GUU"`)
   +	`--comment-id-prefix` _P_: use _P_ as a prefix in front of comment ID numbers (default `"GUC"`)
   +	`--discussion-id` _D_: use _D_ as a discussion ID rather than extracting it from the article URL
-  
-Output:
+  + `--remove-markup`: replace the TXT column with the MD column
+	+ `--columns` _L_: print the column codes in comma-separated list _L_; if any	of the codes are not recognized, the entry is used as a column-heading with NA entries. Column codes are:
+		+	`CID` -- comment ID
+		+	`RCID` -- ID of comment being responded to
+		+	`DPTH` -- 'depth' of the comment
+		+	`AU` -- commenter's displayed name
+		+	`UID` -- pseudonymized user identification number
+		+	`DT` -- date-time of comment
+		+	`UP` -- number of comment recommendations
+		+ `ED` -- comment highlighted by moderator
+		+	`TXT` -- comment text with any HTML markup
+		+	`MD` -- comment text with HTML tagsremoved
+		+	`LVL` -- as RCID, but "Reply to ID" or empty
+	+ `--column-as` _C_ _H_: use _H_ as the heading for column code _C_ (which must be a valid column code unlike for `--columns`) instead of the	default. You can repeat this option as often as you want. Defaults:
+		+	`CID` -- "Comment ID"
+		+	`RCID` -- "Comment ID Responding To"
+		+	`DPTH` -- "Depth"
+		+	`AU` -- "Author"
+		+	`UID` -- "User ID
+		+	`DT` -- "Date Time"
+		+	`UP` -- "Recommendations"
+		+	`ED` -- "Highlighted?"
+		+	`TXT` -- "Comment Text"
+		+	`MD` -- "Comment Text without Markup"
+		+	`LVL` -- "Level"
+	+ `--all`: put all columns in the output, as opposed to the default: `"CID,RCID,DPTH,UID,DT,UP,ED,TXT"`
+
+Default output:
  
 | Comment ID | Comment ID Responding To | User ID | Date Time | Recommendations | Highlighted? | Comment Text |
 | --- | --- | --- | --- | --- | --- | --- |
-| e.g. `GUC001` | `NA` if not a response | e.g. `GUU001` | [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) format | Number of 'upticks' | `1` if highlighted by editor, `0` if not | Text with any HTML markup |
+| e.g. `GUC001` | `NA` if not a response | e.g. `GUU001` | [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) format | Number of 'upticks' | `1` if highlighted by moderator, `0` if not | Text with any HTML markup |
 
 Comments are extracted in the order returned by the JSON output from [The Guardian's discussion API](http://discussion.theguardian.com/discussion-api/). Since responses to comments are embedded in the entry for their comment, this means sorting by comment ID will get you comments then responses row-by-row -- it also means sorting by date-time order will not get you ascending order of comment IDs.
--- graun-btl.pl
+++ graun-btl.pl
@@ -10,10 +10,24 @@ my $uid_pref = "GUU";
 my $cid_pref = "GUC";
 my @p_id;
 my $cmd_p_id = 0;
+my @cols = ("CID", "RCID", "DPTH", "UID", "DT", "UP", "ED", "TXT");
+my %headings = (
+  "CID" => "Comment ID",
+  "RCID" => "Comment ID Responding To",
+  "LVL" => "Level",
+  "DPTH" => "Depth",
+  "UID" => "User ID",
+  "AU" => "Author",
+  "DT" => "Date Time",
+  "UP" => "Recommendations",
+  "ED" => "Highlighted?",
+  "TXT" => "Comment Text",
+  "MD" => "Comment Text without Markup");
 
 binmode STDOUT, ":encoding(UTF-8)";
 
 my $usage_str = "Usage: $0 [--user-id-prefix <string>] [--comment-id-prefix <string>] "
+  ."[--remove-markup] [--columns <column codes>] [--column-as <column code> <heading>] "
   ."{[--discussion-id <string>]|<Guardian Article URL>}";
 
 if(scalar(@ARGV) == 0) {
@@ -32,6 +46,35 @@ while($ARGV[0] =~ /^-/) {
     push(@p_id, shift(@ARGV));
     $cmd_p_id = 1;
   }
+  elsif($opt eq "--remove-markup" || $opt eq "-m") {
+    for(my $i = 0; $i <= $#cols; $i++) {
+      if($cols[$i] eq "TXT") {
+        $cols[$i] = "MD";
+        last;
+      }
+    }
+  }
+  elsif($opt eq "--columns" || $opt eq "-C") {
+    @cols = split(/,/, shift(@ARGV));
+    foreach my $col (@cols) {
+      if(!defined($headings{$col})) {
+        warn "Column code \"$col\" not recognized; will add as heading with NA entries\n";
+      }
+    }
+  }
+  elsif($opt eq "--column-as" || $opt eq "-a") {
+    my $col = shift(@ARGV);
+    my $as = shift(@ARGV);
+    if(defined($headings{$col})) {
+      $headings{$col} = $as;
+    }
+    else {
+      die "Column code \"$col\" not recognized; use --columns for a column with NAs\n";
+    }
+  }
+  elsif($opt eq "--all" || $opt eq "-A") {
+    @cols = ("CID", "RCID", "DPTH", "AU", "UID", "DT", "UP", "ED", "TXT", "MD");
+  }
   else {
     die "$usage_str\n\n"
       ."Download comments from a Guardian article into a CSV format (use output redirect\nto save).\n\n"
@@ -41,7 +84,24 @@ while($ARGV[0] =~ /^-/) {
       ."\t--comment-id-prefix P: use P as a prefix in front of comment ID numbers\n\t\t(default "
       ."\"$cid_pref\")\n"
       ."\t--discussion-id D: use D as a discussion ID rather than extracting it\n\t\tfrom the "
-      ."article URL\n";
+      ."article URL\n"
+      ."\t--remove-markup: replace the TXT column with the MD column\n"
+      ."\t--columns L: print the column codes in comma-separated list L; if any\n\t\tof the "
+      ."codes are not recognized, the entry is used as a column-\n\t\theading with NA entries. "
+      ."Column codes are:\n\t\t\tCID -- comment ID\n\t\t\tRCID -- ID of comment being responded to"
+      ."\n\t\t\tDPTH -- 'depth' of the comment\n\t\t\tAU -- commenter's displayed name\n\t\t\t"
+      ."UID -- pseudonymized user identification number\n\t\t\tDT -- date-time of comment\n\t\t\t"
+      ."UP -- number of comment recommendations\n\t\t\tED -- comment highlighted by moderator"
+      ."\n\t\t\tTXT -- comment text with any HTML markup\n\t\t\tMD -- comment text with HTML tags"
+      ."removed\n\t\t\tLVL -- as RCID, but \"Reply to ID\" or empty\n"
+      ."\t--column-as C H: use H as the heading for column code C instead of the\n\t\tdefault. "
+      ."You can repeat this as often as you want. Defaults:\n\t\t\tCID -- \"$headings{'CID'}\"\n"
+      ."\t\t\tRCID -- \"$headings{'RCID'}\"\n\t\t\tDPTH -- \"$headings{'DPTH'}\"\n\t\t\tAU -- \""
+      ."$headings{'AU'}\"\n\t\t\tUID -- \"$headings{'UID'}\n\t\t\tDT -- \"$headings{'DT'}\"\n\t\t\tUP "
+      ."-- \"$headings{'UP'}\"\n\t\t\tED -- \"$headings{'ED'}\"\n\t\t\tTXT -- \"$headings{'TXT'}\"\n\t\t\t"
+      ."MD -- \"$headings{'MD'}\"\n\t\t\tLVL -- \"$headings{'LVL'}\"\n"
+      ."\t--all: put all columns in the output, as opposed to the default:\n\t\t\""
+      .(join(",", @cols))."\"\n";
   }
 }
 
@@ -116,9 +176,10 @@ while(scalar(keys(%comments)) < $n_cmt) {
     my $dt = $json->{'discussion'}->{'comments'}->[$i]->{'isoDateTime'};
     my $hi = $json->{'discussion'}->{'comments'}->[$i]->{'isHighlighted'};
     my $rec = $json->{'discussion'}->{'comments'}->[$i]->{'numRecommends'};
+    my $uname = $json->{'discussion'}->{'comments'}->[$i]->{'userProfile'}->{'displayName'};
     my $n_resp = $json->{'discussion'}->{'comments'}->[$i]->{'metaData'}->{'responseCount'};
 
-    $comments{$id} = [$id, $uid, $dt, $rec, $hi, $txt];
+    $comments{$id} = [$id, $uid, $dt, $rec, $hi, $txt, $uname];
     $cids{$id} = ++$nxt_cid if !defined($cids{$id});
     $uids{$uid} = ++$nxt_uid if !defined($uids{$uid});
 
@@ -129,9 +190,10 @@ while(scalar(keys(%comments)) < $n_cmt) {
       my $resp_dt = $json->{'discussion'}->{'comments'}->[$i]->{'responses'}->[$j]->{'isoDateTime'};
       my $resp_hi = $json->{'discussion'}->{'comments'}->[$i]->{'responses'}->[$j]->{'isHighlighted'};
       my $resp_rec = $json->{'discussion'}->{'comments'}->[$i]->{'responses'}->[$j]->{'numRecommends'};
+      my $resp_un = $json->{'discussion'}->{'comments'}->[$i]->{'responses'}->[$j]->{'userProfile'}->{'displayName'};
 
       $responses{$resp_id} = $id;
-      $comments{$resp_id} = [$resp_id, $resp_uid, $resp_dt, $resp_rec, $resp_hi, $resp_txt];
+      $comments{$resp_id} = [$resp_id, $resp_uid, $resp_dt, $resp_rec, $resp_hi, $resp_txt, $resp_un];
       $cids{$resp_id} = ++$nxt_cid if !defined($cids{$resp_id});
       $uids{$resp_uid} = ++$nxt_uid if !defined($uids{$resp_uid});
     }
@@ -140,25 +202,92 @@ while(scalar(keys(%comments)) < $n_cmt) {
   $pp++;
 }
 
+my %depths;
+my $depth = 0;
+my $n = 0;
+do {
+  $n = 0;
+  foreach my $id (keys(%comments)) {
+    if($depth > 0 && defined($responses{$id}) && defined($depths{$responses{$id}})) {
+      $depths{$id} = 1 + $depths{$responses{$id}};
+    }
+    elsif($depth == 0 && !defined($responses{$id})) {
+      $depths{$id} = $depth;
+      $n++;
+    }
+  }
+  $depth++;
+} while($n > 0);
+
 my $nc_dig = length(sprintf("%d", scalar(keys(%cids))));
 my $nu_dig = length(sprintf("%d", scalar(keys(%uids))));
 
-print "Comment ID,Comment ID Responding To,User ID,Date Time,Recommendations,Highlighted?,Comment Text\n";
+foreach my $col (@cols) {
+  if(defined($headings{$col})) {
+    print $headings{$col};
+  }
+  else {
+    print $col;
+  }
+  print ($col eq $cols[$#cols] ? "\n" : ",");
+}
 foreach my $cid (sort(keys(%comments))) {
-  my ($id, $uid, $dt, $rec, $hi, $txt) = @{$comments{$cid}};
+  my ($id, $uid, $dt, $rec, $hi, $txt, $au) = @{$comments{$cid}};
 
   next if $id !~ /./;
 
   $txt =~ s/\n/ /g;
+  my $md = $txt;
+  $md =~ s/\<[^\>]+\>/ /g;
   $txt =~ s/\"/\"\"/g;
   $txt = "\"$txt\"";
+  $md =~ s/\"/\"\"/g;
+  $md = "\"$md\"";
 
   my $pcid = sprintf("${cid_pref}%0*d", $nc_dig, $cids{$id});
   my $puid = sprintf("${uid_pref}%0*d", $nu_dig, $uids{$uid});
 
   my $resp_to = (defined($responses{$id}) ? sprintf("${cid_pref}%0*d", $nc_dig, $cids{$responses{$id}}) : "NA");
 
-  print "$pcid,$resp_to,$puid,$dt,$rec,$hi,$txt\n"
+  foreach my $col (@cols) {
+    if($col eq "CID") {
+      print $pcid;
+    }
+    elsif($col eq "RCID") {
+      print $resp_to;
+    }
+    elsif($col eq "LVL") {
+      print ($resp_to eq "NA" ? "" : "Reply to $resp_to");
+    }
+    elsif($col eq "DPTH") {
+      print $depths{$id};
+    }
+    elsif($col eq "UID") {
+      print $puid;
+    }
+    elsif($col eq "AU") {
+      print $au;
+    }
+    elsif($col eq "DT") {
+      print $dt;
+    }
+    elsif($col eq "UP") {
+      print $rec;
+    }
+    elsif($col eq "ED") {
+      print $hi;
+    }
+    elsif($col eq "TXT") {
+      print $txt;
+    }
+    elsif($col eq "MD") {
+      print $md;
+    }
+    else {
+      print "NA";
+    }
+    print ($col eq $cols[$#cols] ? "\n" : ",");
+  }
 }
 
 if(scalar(keys(%comments)) < $n_cmt) {
